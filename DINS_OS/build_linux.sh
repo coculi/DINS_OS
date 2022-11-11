07b6a1dcf553c473c19de1f6883b020eb91edfbe
Dineshika <91914577+DineshikaSivapalaraja@users.noreply.github.com>
2022-11-12 00:18:05 +0530

    Add files via upload

---
+++ DINS_OS/build_linux.sh
@@ -0,0 +1,67 @@
+#!/bin/sh
+
+# This script assembles the DINS_OS bootloader, kernel and programs
+# with NASM, and then creates floppy and CD images (on Linux)
+
+
+
+if test "`whoami`" != "root" ; then
+	echo "You must be logged in as root to build (for loopback mounting)"
+	echo "Enter 'su' or 'sudo bash' to switch to root"
+	exit
+fi
+
+
+if [ ! -e disk_images/dins_OS.flp ]
+then
+	echo ">>> Creating new DINS_OS floppy image..."
+	mkdosfs -C disk_images/dins_OS.flp 1440 || exit
+fi
+
+
+echo ">>> Assembling bootloader..."
+
+nasm -O0 -w+orphan-labels -f bin -o source/bootload/bootload.bin source/bootload/bootload.asm || exit
+
+
+echo ">>> Assembling DINS_OS kernel..."
+
+cd source
+nasm -O0 -w+orphan-labels -f bin -o kernel.bin kernel.asm || exit
+cd ..
+
+
+echo ">>> Assembling programs..."
+
+
+
+
+
+echo ">>> Adding bootloader to floppy image..."
+
+dd status=noxfer conv=notrunc if=source/bootload/bootload.bin of=disk_images/dins_OS.flp || exit
+
+
+echo ">>> Copying DINS_OS kernel and programs..."
+
+rm -rf tmp-loop
+
+mkdir tmp-loop && mount -o loop -t vfat disk_images/dins_OS.flp tmp-loop && cp source/kernel.bin tmp-loop/
+
+
+
+sleep 0.2
+
+echo ">>> Unmounting loopback floppy..."
+
+umount tmp-loop || exit
+
+rm -rf tmp-loop
+
+
+echo ">>> Creating CD-ROM ISO image..."
+
+rm -f disk_images/dins_OS.iso
+mkisofs -quiet -V 'DINS_OS' -input-charset iso8859-1 -o disk_images/dins_OS.iso -b dins_OS.flp disk_images/ || exit
+
+echo '>>> Done!'
